5a788eefdaea9783bcc3f2f94bd6d08e2cf3e58c
sabrainas <mendoncasabrina61@gmail.com>
2023-02-15 18:33:45 -0300

    mais aulas adicionadas

---
+++ JAVASCRIPT/aula03.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/*function teste(){
+    let nome="Sabrina"
+    if(true){
+        console.log("dentro do if teste: " + nome)
+    }
+    console.log("dentro da função teste: " + nome)
+}*/
+
+let nome = "Sabrina"
+nome = "Mendonça"
+nome=19
+
+const curso = "JavaScript" //nao é possivel alterar o valor de uma cons
+
+//teste()
+
+//console.log("fora do teste" + nome);
+
+//dentro da função o var continua dando erro 
