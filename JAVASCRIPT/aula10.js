3dc7d4a0d0e504361915c51b9b2fa70ecb833787
sabrainas <sazonass.m@gmail.com>
2023-03-01 10:42:17 -0300

    mais aulas 01/03/2023

---
+++ JAVASCRIPT/aula10.js
@@ -0,0 +1,19 @@
+let num=10
+let num2=5
+res=(!(num%2) ? "Par" : "Impar")
+
+res2=(num > num2 ? "Verdadeiro" : "Falso")
+
+console.log(res)
+console.log(res2)
+
+//0 = false
+//1 = true
+
+//teste lógico ? se verdadeiro : se falso
+
+// if(res==0){
+//     console.log("Par")
+// }else{
+//     console.log("Impar")
+// }
